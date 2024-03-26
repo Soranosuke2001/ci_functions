48ae46e002912508df8ca10203b81d59cc9e7765
Sora <80365991+Soranosuke2001@users.noreply.github.com>
2024-03-25 22:27:07 -0700

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -12,11 +12,9 @@ def call(dockerRepoName, imageName) {
 
                     sh 'python3 -m venv venv'
                     sh '. venv/bin/activate'
-
-                    sh 'echo $USER'
                     
-                    sh 'pip install -r requirements.txt --break-system-packages'
-                    sh 'pip install --upgrade flask --break-system-packages'
+                    sh 'pip install -r requirements.txt'
+                    sh 'pip install --upgrade flask'
                 }
             }
             stage('Python Lint') {
@@ -43,10 +41,8 @@ def call(dockerRepoName, imageName) {
                         // Run tests with coverage
                         sh '. venv/bin/activate'
 
-                        def testFiles = findFiles(glob: 'test*.py')
-                        testFiles.each {
-                            sh "coverage run --omit '*/site-packages/*,*/dist-packages/*' ${it.path}"
-                        }
+                        // Use shell commands to find test files
+                        sh 'find . -name "test*.py" | xargs coverage run --omit "*/site-packages/*,*/dist-packages/*"'
                     }
                     
                     script {
@@ -70,10 +66,10 @@ def call(dockerRepoName, imageName) {
                 }
                 steps {
                     withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
-                    sh "docker login -u 'soranosuke' -p '$TOKEN' docker.io"
-                    sh "docker build -t ${dockerRepoName}:latest --tag soranosuke/${dockerRepoName}:${imageName} ."
-                    sh "docker push soranosuke/${dockerRepoName}:${imageName}"
-                }
+                        sh "docker login -u 'soranosuke' -p '$TOKEN' docker.io"
+                        sh "docker build -t ${dockerRepoName}:latest --tag soranosuke/${dockerRepoName}:${imageName} ."
+                        sh "docker push soranosuke/${dockerRepoName}:${imageName}"
+                    }
                 }
             }
             stage('Zip Artifacts') {
