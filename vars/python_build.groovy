12baeb2f31ada0e360b5dd85d9e0ec057265e548
Soranosuke2001 <sorazora1.4@gmail.com>
2024-03-25 23:00:56 -0700

    updated groovy file

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -2,27 +2,37 @@ def call(dockerRepoName, imageName) {
     pipeline {
         agent any
         stages {
-            stage('Build') {
+            stage('Setup') {
                 steps {
                     script {
-                        if (fileExists('venv')) {
-                            sh 'rm -rf venv'
-                        }
+                        // Create and activate virtual environment
+                        sh 'python3 -m venv venv'
+                        sh '. venv/bin/activate'
+                        // Upgrade pip to avoid potential issues
+                        sh 'pip install --upgrade pip'
+                        // Install flask and other dependencies
+                        sh 'pip install --upgrade flask'
+                        sh 'pip install -r requirements.txt'
                     }
-
-                    sh 'python3 -m venv venv'
-                    sh '. venv/bin/activate'
-
-                    sh 'pip install --upgrade flask'
-                    sh 'pip install -r requirements.txt'
                 }
             }
-            stage ('PyLint') {
+            stage ('Lint') {
                 steps {
                     script {
-                        sh 'pylint Receiver/*.py'
+                        // Run pylint
+                        sh 'pylint Receiver/*.py || true' // Continue pipeline execution even if pylint fails
                     }
                 }  
+            }
+            // Add other stages as per your requirements
+        }
+        post {
+            always {
+                script {
+                    // Clean up virtual environment
+                    sh 'deactivate || true' // Deactivate virtual environment
+                    sh 'rm -rf venv' // Remove virtual environment
+                }
             }
         }
     }
