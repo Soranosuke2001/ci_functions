9394cb90e9629bb43f17e6b3f6916089f1ba8d00
Sora <80365991+Soranosuke2001@users.noreply.github.com>
2024-03-25 22:12:08 -0700

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -13,8 +13,8 @@ def call(dockerRepoName, imageName) {
                     sh 'python3 -m venv venv'
                     sh '. venv/bin/activate'
                     
-                    sh 'pip install -r requirements.txt'
-                    sh 'pip install --upgrade flask'
+                    sh 'pip install -r requirements.txt --break-system-packages'
+                    sh 'pip install --upgrade flask --break-system-packages'
                 }
             }
             stage('Python Lint') {
